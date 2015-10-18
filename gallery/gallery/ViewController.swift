ab1e5c26a9dc4067cf7c118ac90ff2ddee70b000
ouyongyong <ouyongyong@ouyongyongdeMacBook-Pro.local>
2015-10-18 12:45:58 +0800

    create proj

---
+++ gallery/gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gallery
+//
+//  Created by ouyongyong on 15/10/18.
+//  Copyright © 2015年 kira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
